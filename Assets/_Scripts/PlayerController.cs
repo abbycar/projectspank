24251aa5401d46b7b92234648d50f8267b7de484
Blairinks <jeffjohn@uw.edu>
2014-12-18 14:57:26 -0800

    Spanky can Slide
    Added some background and basic controls

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float PlayerSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		
+		
+		Vector2 controlMovement = new Vector2(moveHorizontal * PlayerSpeed, rigidbody2D.velocity.y);
+		
+		rigidbody2D.velocity = controlMovement;
+
+	}
+}
